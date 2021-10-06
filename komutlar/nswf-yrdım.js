c828b0c03a30d7123e2990e9b63e9b9852e8b09a
Glitch (dust-holy-cafe) <none>
2021-10-06 14:11:58 +0000

    🌉🌕 Checkpoint ./komutlar/nswf-yrdım.js:36950787/926

--- komutlar/nswf-yrdım.js
+++ komutlar/nswf-yrdım.js
@@ -6,17 +6,12 @@ const ab = new Discord.MessageEmbed()
 .setURL('https://discord.com/oauth2/authorize?client_id=890967082555019315&scope=bot&permissions=805314622')
 .setTitle('☂️Shaka bot Nswf yardım menüsü')
 .addField('<:ilri:895072006750490634>s+hentai-feet', 'rastgele hentai porno resmi atar', true)
-.addField('<:ilri:895072006750490634>s+pgif', 'belirttiğiniz sebepten afk olursunuz.', true)
-.addField('<:ilri:895072006750490634>s+ping', 'porno gifi atar', true)
-.addField('<:ilri:895072006750490634>s+sunucubilgi', 'sunucu hakkında bilgi verir', true)
-.addField('<:ilri:895072006750490634>s+sunucuresmi', 'sunucunun resmini atar', true)
-.addField('<:ilri:895072006750490634>s+davet', 'tüm davet linklerini atar', true)
-.addField('<:ilri:895072006750490634>s+sayı-tahmin', 'çıkan sayıyı tahmin etmeye çalışırsınız', true)
-.addField('<:ilri:895072006750490634>s+not', 'not alırsınız', true)
-.addField('<:ilri:895072006750490634>s+zar-at', 'zar atarsınız', true)
-.addField('<:ilri:895072006750490634>s+hata', 'bot sahibine botta olan hatayı bildirirsiniz', true)
-.addField('<:ilri:895072006750490634>s+banner', 'etiketlediğiniz kişinin bannerini atar', true)
-.setImage("https://cdn.discordapp.com/attachments/856935661902626848/893872633064615967/Shaka.png")
+.addField('<:ilri:895072006750490634>s+pgif', 'porno gifi atar.', true)
+.addField('<:ilri:895072006750490634>s+ass', 'rastgele porno gifi atar', true)
+.addField('<:ilri:895072006750490634>s+hentai-thigs', 'hentai porno atar', true)
+.addField('<:ilri:895072006750490634>s+panties', 'restgele değişik porno giflerş atar', true)
+.addField('<:ilri:895072006750490634>s+thighs', 'porno resmi atar', true)
+.setImage("https://cdn.discordapp.com/attachments/881203219567640647/895311530168553502/nsfw-1200x675.jpg")
 .setThumbnail("https://cdn.discordapp.com/attachments/856935661902626848/893872633064615967/Shaka.png")
 .setFooter('☂️Shaka bot', 'https://cdn.discordapp.com/avatars/890967082555019315/cba9f99227093d53d7cf4566113eb9d3.png?size=4096');
 
@@ -33,7 +28,7 @@ module.exports.conf = {
 };
 
 module.exports.help = {
-  name: "kyardım",
+  name: "nyardım",
   description: "",
   usage: ""
 };
